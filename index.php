35d5161456286b761d9dc3d2dd1c1a15b6d1f945
3A532067 <s3a532067@student.ncut.edu.tw>
2018-10-30 20:45:59 +0800

    另一種require autoload.php的方式

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 
 require __DIR__.'/bootstrap.php';
-require './vendor/autoload.php';
+//require './vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
 
 //date_default_timezone_set('Asia/Taipei');
 
